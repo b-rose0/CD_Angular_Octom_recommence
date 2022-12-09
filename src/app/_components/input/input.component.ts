f3bc9f0560887e0576d247bd3cf9eb2909be520f
Berenice Rose <berenice.rose0@gmail.com>
2022-12-09 16:14:17 +0100

    ajout de la visibilité pour input password

--- src/app/_components/input/input.component.ts
+++ src/app/_components/input/input.component.ts
@@ -1,17 +1,22 @@
-import { Component, EventEmitter, Input, Output  } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output  } from '@angular/core';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss']
 })
-export class InputComponent {
+export class InputComponent implements OnInit{
+
   @Input() label!: string;
   @Input() placeHolder: string = "";
   @Input() typeOfInput: string = "text";
+  isPassword!: boolean;
 
   @Output() inputEmmitter: EventEmitter<string> = new EventEmitter();
 
+  ngOnInit(): void {
+    this.isPassword = this.typeOfInput == "password";
+  }
 
   displayInputValue(value: string) {
     console.log("Input "+this.label+" value is "+value);
@@ -19,5 +24,13 @@ export class InputComponent {
 
   emitInput(value: string) {
     this.inputEmmitter.emit(value);
+  }
+
+  togglePassword() {
+    if(this.typeOfInput == "password") {
+      this.typeOfInput = "text";
+    } else {
+      this.typeOfInput = "password";
+    }
   }
 }
